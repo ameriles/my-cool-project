6be204d65d38cd5abd836a793e2858faf19de010
Agustin Meriles <jameriles@gmail.com>
2023-02-07 10:15:33 -0300

    #4 | Change datetime text (#5)
    * #4 | Change datetime text
    
    * #4 | Updated Date & time

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main(void) {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("Date & Time: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
     printf("Hello, you All!\n");
     return 0;
